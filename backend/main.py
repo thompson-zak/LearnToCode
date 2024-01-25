7bf1ff66306a75e4b48464a5b9311ab32688710d
Zak Thompson <zakthompson97@gmail.com>
2024-01-25 15:19:31 -0600

    Basic integration with OpenAI completions api

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,63 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Header
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from openai import OpenAI
 
+
+class Settings(BaseSettings):
+    openai_api_key: str
+    frontend_api_auth: str
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+settings = Settings()
 app = FastAPI()
+client = OpenAI(
+    api_key=settings.openai_api_key
+)
 
+prompts = {
+    "Variables": {
+        1: {
+
+        },
+        2: {
+
+        },
+        3: {
+
+        }
+    },
+    "Data": {},
+    "Conditionals": {},
+    "OOP": {}
+}
+
+# 
+# API used from front-end to call for ChatGPT responses to fill the various exercises provided to students
+#
+# Section - this is the title of the section that the user is currently on
+# Id - this is the specific exercise within the section that needs to be populated with data
+# Auth_header - this will be used to identify requests and deny users
+#
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root(section: str, id: int, auth_header: Annotated[str | None, Header()] = None):
+
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a computer science professor, skilled in explaining complex programming concepts to beginners."},
+            {"role": "user", "content": "Please provide a prompt and code outline to help a student who has never coded before learn python syntax and variables.."}
+        ]
+    )
+
+    return {
+        "completion": completion
+    }
+
+
+@app.get("/test")
+async def test():
+    return {
+        "message": "Hello World!"
+    }
