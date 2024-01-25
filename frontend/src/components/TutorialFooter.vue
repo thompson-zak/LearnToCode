b45ee79900c735eded5000b386eddd1e0ba4fa63
Zak Thompson <zakthompson97@gmail.com>
2024-01-25 08:12:58 -0600

    Fix indenting. Extract footer into separate component.

---
+++ frontend/src/components/TutorialFooter.vue
@@ -0,0 +1,34 @@
+<script setup>
+</script>
+
+<template>
+    <div class="max-w-6xl w-full mt-4">
+      <RouterLink
+        to="/"
+        class="float-left group rounded-lg border border-transparent px-4 py-3 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+      >
+        <h2 class="mb-2 text-2xl font-semibold">
+          <IconSvg name="arrow-left-long" size="25px" class="inline-block"></IconSvg>
+          Home
+        </h2>
+        <p class="m-0 max-w-[30ch] text-sm opacity-50 inline-block">
+          Go back to start another lesson.
+        </p>
+      </RouterLink>
+
+      <div class="float-right group rounded-lg border border-transparent px-4 py-3 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+        <a
+          class="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0 inline-block"
+          href="https://www.markcubanai.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          By
+          <img src="../assets/cuban-logo.png" class="h-6 ml-2 inline-block"/>
+        </a>
+      </div>
+    </div>
+</template>
+
+<style>
+</style>
--- frontend/src/views/VariablesView.vue
+++ frontend/src/views/VariablesView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import TutorialTab from './../components/TutorialTab.vue'
 import TutorialContent from './../components/TutorialContent.vue'
+import TutorialFooter from './../components/TutorialFooter.vue'
 import { ref } from 'vue';
 
 const SECTION_DESCRIPTION = "These exercises are designed to help you understand the syntax of python and introduce you to the concept of variables."
@@ -66,35 +67,8 @@ const display = ref(1);
         </div>
       </div>
     </div>
-    <div class="max-w-6xl w-full mt-4">
-      <a
-        href="/"
-        class="float-left group rounded-lg border border-transparent px-4 py-3 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-      >
-        <h2 class="mb-3 text-2xl font-semibold">
-          <IconSvg name="arrow-left-long" size="25px" class="inline-block"></IconSvg>
-          Home
-        </h2>
-        <p class="m-0 max-w-[30ch] text-sm opacity-50 inline-block">
-          Go back to start another lesson.
-        </p>
-      </a>
-
-      <div class="float-right">
-        <a
-          class="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0 inline-block"
-          href="https://www.markcubanai.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          By
-          <a>
-            <img src="../assets/cuban-logo.png" class="h-6 ml-2 inline-block"/>
-          </a>
-        </a>
-      </div>
-    </div>
-    </main>
+    <TutorialFooter />
+  </main>
 </template>
 
 <style>
